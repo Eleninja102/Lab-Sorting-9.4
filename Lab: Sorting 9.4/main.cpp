0f070867c3a4f1b0d22915c9b4e8fb4288dfa920
Coleton Watt <electricninja102@gmail.com>
2022-03-17 09:23:26 -0600

    Initial Commit

---
+++ Lab: Sorting 9.4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab: Sorting 9.4
+//
+//  Created by Coleton Watt on 3/17/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
